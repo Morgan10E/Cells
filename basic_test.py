d08fd2366913f1074e721642711e3c4b1cb25c65
Morgan Tenney <morgantenney@Morgans-MacBook-Pro.local>
2016-10-27 22:13:37 -0700

    Basic test reads in cell_counts.csv

--- basic_test.py
+++ basic_test.py
@@ -1,6 +1,9 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 import numpy
+import csv
+from PIL import Image
+from pybrain.supervised.trainers import BackpropTrainer
 
 def getFeaturesFromFile(filename):
 	im = Image.open(filename).convert('L')
@@ -14,13 +17,23 @@ net = buildNetwork(numPixels, 100, 1)
 # create the dataset
 ds = SupervisedDataSet(numPixels, 1)
 
+#find counts and image filenames
+dataCSV = open('cell_counts.csv')
+reader = csv.reader(dataCSV)
+data = {}
+for row in reader:
+	data[row[0]] = float(row[1])
+
 # add the files to the dataset
-ds.addSample(features, (numCells,))
+for key, numCells in data.items():
+	ds.addSample(getFeaturesFromFile(key), (numCells,))
 
 # train the network
 trainer = BackpropTrainer(net, ds)
-for i in range(10):
+for i in range(100):
 	print trainer.train()
 
 # activate the net on the dataset to see what we get
-net.activate(features)
+for key, numCells in data.items():
+	guess = net.activate(getFeaturesFromFile(key))
+	print guess, numCells
